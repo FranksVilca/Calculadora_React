5aeec030228aa9ed53dd3209337cc7acb665b27d
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-19 18:44:16 -0500

    Creado primera parte del esqueleto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo (1).png'
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodecamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
